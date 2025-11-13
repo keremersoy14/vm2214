26c56e717d8ee844a0f80fa187c76ed73cf6d152
kerem <90874600+keremersoy14@users.noreply.github.com>
2025-11-13 12:14:12 +0300

    Update asd.py

--- asd.py
+++ asd.py
@@ -1,1 +1,5 @@
-
+curl -s \
+  -H "Auth-Key: YOUR-AUTH-KEY-HERE" \
+  -d "query=get_file_type&file_type=pdf&limit=1000" \
+  https://mb-api.abuse.ch/api/v1/ \
+  | jq -r '.data[].sha256_hash' > pdf_sha256.txt
